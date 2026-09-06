363eca74503d917cf632fc6c468a9793cc6f0202
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-06 23:47:39 +0530

    Synchronize Streamlit universal search with additive source-discovery expansion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-from software.mlhkp_knowledge_engine import render
+import software.mlhkp_knowledge_engine as engine
 
-render()
+engine.WEB = engine.WEB + engine.load(
+    'data/source_census/web_discovery_expansion_2026-09-06_run2.json',
+    {},
+).get('records', [])
+
+engine.render()
