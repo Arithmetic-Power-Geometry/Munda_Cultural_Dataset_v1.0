70ee23b6f6920c383e3c8cc3c766e5616f99846c
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-06 17:05:49 +0530

    Test publication comparison matrix and verified references

--- tests/test_publication_sync.py
+++ tests/test_publication_sync.py
@@ -41,13 +41,40 @@ def test_manuscript_uses_generated_metrics_and_truthful_completion_language():
 
 def test_manuscript_caption_and_citation_order_contract():
     tex = (ROOT / "publication/main.tex").read_text(encoding="utf-8")
-    assert tex.index("Table~\\ref{tab:release}") < tex.index("\\begin{table}")
+    first_table = tex.index("\\begin{table}")
+    second_table = tex.index("\\begin{table}", first_table + 1)
+    assert tex.index("Table~\\ref{tab:release}") < first_table
+    assert tex.index("Table~\\ref{tab:contribution}") < second_table
     assert tex.index("Algorithm~\\ref{alg:census}") < tex.index("\\begin{algorithm}")
     assert tex.index("Algorithm~\\ref{alg:audit}") < tex.rindex("\\begin{algorithm}")
-    table = tex[tex.index("\\begin{table}"):tex.index("\\end{table}")]
-    algorithm = tex[tex.index("\\begin{algorithm}"):tex.index("\\end{algorithm}")]
-    assert table.index("\\caption") < table.index("\\begin{tabular}")
-    assert algorithm.index("\\caption") < algorithm.index("\\begin{algorithmic}")
+
+    release_table = tex[first_table:tex.index("\\end{table}", first_table)]
+    contribution_table = tex[second_table:tex.index("\\end{table}", second_table)]
+    first_algorithm = tex[tex.index("\\begin{algorithm}"):tex.index("\\end{algorithm}")]
+    assert release_table.index("\\caption") < release_table.index("\\begin{tabular}")
+    assert contribution_table.index("\\caption") < contribution_table.index("\\begin{tabular}")
+    assert first_algorithm.index("\\caption") < first_algorithm.index("\\begin{algorithmic}")
+
+
+def test_verified_comparison_bibliography_and_matrix_are_present():
+    tex = (ROOT / "publication/main.tex").read_text(encoding="utf-8")
+    bib = (ROOT / "publication/references.bib").read_text(encoding="utf-8")
+    for key in (
+        "christen2015localcontexts",
+        "dutta2022mukurtu",
+        "faraj2021cidoc",
+        "williamson2023sovereignty",
+    ):
+        assert key in tex
+        assert f"@article{{{key}," in bib
+    for doi in (
+        "10.26077/78d5-47cf",
+        "10.17821/srels/2022/v59i2/168787",
+        "10.3390/fi13110277",
+        "10.1177/26349825221125496",
+    ):
+        assert doi in bib
+    assert "The entries describe design emphasis, not claims of superiority" in tex
 
 
 def test_foundational_bibliography_entries_are_not_placeholders():
